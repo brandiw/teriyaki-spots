93f626274d2c4f435788d7b3b9fe47d8459ab415
brandiw <brandimichelle.0911@gmail.com>
2019-04-15 15:39:32 -0700

    Added review model

---
+++ models/review.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const review = sequelize.define('review', {
+    name: DataTypes.STRING,
+    comment: DataTypes.TEXT,
+    rating: DataTypes.DECIMAL,
+    spotId: DataTypes.INTEGER
+  }, {});
+  review.associate = function(models) {
+    // associations can be defined here
+  };
+  return review;
+};
